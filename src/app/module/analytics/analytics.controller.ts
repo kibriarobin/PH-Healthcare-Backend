3e9f41d12b57d601710c7b7751e1e4c92cdaf940
Golam Kibria <kibriarobin97@gmail.com>
2026-09-01 22:54:40 +0600

    added analytics controller

---
+++ src/app/module/analytics/analytics.controller.ts
@@ -0,0 +1,45 @@
+import httpStatus from "http-status";
+import { sendResponse } from "../../utils/sendResponse";
+import { AnalyticsService } from "./analytics.route";
+import { catchAsync } from "../../utils/catchAsync";
+import type { Request, Response } from "express";
+
+const getAdminAnalytics = catchAsync(async (req: Request, res: Response) => {
+  const result = await AnalyticsService.getAdminAnalytics();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Analytics Retrieved Successfully",
+    data: result,
+  });
+});
+
+const getPatientAnalytics = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user!;
+
+  const result = await AnalyticsService.getPatientAnalytics(user);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Patient Analytics Retrieved Successfully",
+    data: result,
+  });
+});
+
+const getDoctorAnalytics = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user!;
+
+  const result = await AnalyticsService.getDoctorAnalytics(user);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Doctor Analytics Retrieved Successfully",
+    data: result,
+  });
+});
+
+export const AnalyticsController = {
+  getAdminAnalytics,
+  getPatientAnalytics,
+  getDoctorAnalytics,
+};
